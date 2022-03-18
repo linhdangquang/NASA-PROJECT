f4e93063947d5cd880964c64e0f5bfee3c7905c8
DLinh <nogamenolife.why@gmail.com>
2022-03-19 00:34:31 +0700

    feat: static frontend

--- server/src/app.js
+++ server/src/app.js
@@ -1,13 +1,16 @@
-import express from 'express';
-import cors from 'cors';
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
 import planetsRouter from './routes/planets/planets.router';
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:3000',
-}));
+app.use(cors());
 
 app.use(express.json());  
+app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(planetsRouter);
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..' ,  'index.html'));
+});
 
 export default app;
