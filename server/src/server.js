91fbfd8cc5156152e8dd5a9c0f7b0920bbb59ec8
DLinh <nogamenolife.why@gmail.com>
2022-03-23 12:05:50 +0700

    feat: mongodb config

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,25 @@
 import app from './app';
+const mongoose = require('mongoose');
 const http = require('http');
 const PORT = process.env.PORT || 4000;
+const MONGO_URL = 'mongodb+srv://admin:111112113aA@nasa.dtsdf.mongodb.net/nasa?retryWrites=true&w=majority'
+
 const {loadPlanetsData } = require('./models/planets.model');
 const server = http.createServer(app);
 
+mongoose.connection.on('open', () => {
+  console.log('MongoDB is connected');
+})
+
+mongoose.connection.on('error', (err) => {
+  console.log(err);
+})
+
 async function start() {
+  mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   await loadPlanetsData();
   server.listen(PORT);
   console.log(`Server running on port ${PORT}`);
